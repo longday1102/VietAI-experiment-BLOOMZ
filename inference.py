b004f9a172b75fc889833829cf35866db3059193
Hoàng Long <longhoangai1102@gmail.com>
2023-08-04 02:59:17 +0700

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,37 +1,43 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from peft import PeftConfig, LoraConfig, PeftModel, get_peft_model
 import torch
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+import os
+from torch.nn.parallel import DistributedDataParallel as DDP
 
 class Inference:
     def __init__(self,
                  prompt_process,
                  model_checkpoint: str,
                  model_for_tokenizer: str,
+                 gpu_id,
                  model_from_hub: str = None,
                  model_from_checkpoint = None,
                  from_hub: bool = False,
-                 from_checkpoint: bool = False,
-                 device = device):
+                 from_checkpoint: bool = False):
         
         self.prompt_process = prompt_process
-        self.device = device
         self.tokenizer = AutoTokenizer.from_pretrained(model_for_tokenizer)
         if from_checkpoint is True and from_hub is False:
-            self.model = AutoModelForCausalLM.from_pretrained(model_checkpoint, load_in_8bit = True, torch_dtype = torch.float16, device_map = "auto")
+            self.model = AutoModelForCausalLM.from_pretrained(model_checkpoint, device_map = {"": torch.device(f"cuda:{gpu_id}")}).half()
             lora_config = LoraConfig(r = 8,
                                     lora_alpha = 16,
                                     lora_dropout = 0.05,
                                     bias = "none",
                                     task_type = "CAUSAL_LM")
             self.model = get_peft_model(self.model, lora_config)
+            self.model.to(f"cuda:{gpu_id}")
             self.model.load_state_dict(model_from_checkpoint["model_state_dict"])
+            self.model = DDP(self.model, device_ids = [self.gpu_id])
         elif from_checkpoint is False and from_hub is True:
             config = PeftConfig.from_pretrained(model_from_hub)
-            self.model = AutoModelForCausalLM.from_pretrained(config.base_model_name_or_path, load_in_8bit = True, torch_dtype = torch.float16, device_map = "auto")
+            self.model = AutoModelForCausalLM.from_pretrained(config.base_model_name_or_path, device_map = {"": torch.device(f"cuda:{gpu_id}")}).half()
             self.model = PeftModel.from_pretrained(self.model, model_from_hub)
-        self.model.to(device)
+            self.model = DDP(self.model, device_ids = [self.gpu_id])
+        self.gpu_id = gpu_id
+                     
+    def is_master_process(self):
+        ddp_rank = int(os.environ['RANK'])
+        return ddp_rank == 0
 
     def __call__(self,
                  instruction: str,
@@ -39,7 +45,7 @@ class Inference:
                  target: str = None):
         prompt = self.prompt_process.generate_prompt(instruction = instruction, input = input)
         inputs = self.tokenizer(prompt, return_tensors = "pt")
-        inputs = {k:v.to(self.device) for k, v in inputs.items()}
+        inputs = {k:v.to(self.gpu_id) for k, v in inputs.items()}
         outputs = self.model.generate(**inputs,
                                       max_new_tokens = 512,
                                       no_repeat_ngram_size = 3,
@@ -52,9 +58,10 @@ class Inference:
                                       early_stopping = True)
         text = self.tokenizer.batch_decode(outputs, skip_special_tokens = True)[0]
         response = self.prompt_process.get_response(text)
-        if target is not None:
-            return {"target": target,
-                  "response": response}
-        else:
-            return {"response": response}
+        if self.is_master_process():
+            if target is not None:
+                return {"target": target,
+                        "response": response}
+            else:
+                return {"response": response}
         
