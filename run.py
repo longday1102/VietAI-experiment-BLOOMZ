6218197458c7df3dff6b86df5db185537a6279dd
Hoàng Long <longhoangai1102@gmail.com>
2023-08-03 10:42:24 +0700

    Update run.py

--- run.py
+++ run.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
     dataset = data_prcess.load_data()
     prompter = Prompter()
 
-    splited_dataset = dataset.train_test_split(test_size = 0.05, seed = 42)
+    splited_dataset = dataset.train_test_split(test_size = 0.99, seed = 42)
 
     # Model inputs
     model_inputs = MODEL_INPUTS(prompter =  prompter,
