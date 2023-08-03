0749e965bd34027e8c22b201d463f3407c6f467c
Hoàng Long <longhoangai1102@gmail.com>
2023-08-03 22:15:24 +0700

    Add files via upload

---
+++ inference.py
@@ -0,0 +1,62 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from peft import PeftConfig, LoraConfig, PeftModel, get_peft_model
+import torch
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+class Inference:
+    def __init__(self,
+                 prompt_process,
+                 model_checkpoint: str,
+                 model_for_tokenizer: str,
+                 model_from_hub: str = None,
+                 model_from_checkpoint: str = None,
+                 from_hub: bool = False,
+                 from_checkpoint: bool = False,
+                 device = device):
+        
+        self.prompt_process = prompt_process
+        self.device = device
+        self.tokenizer = AutoTokenizer.from_pretrained(model_for_tokenizer)
+        if from_checkpoint is True and from_hub is False:
+            self.model = AutoModelForCausalLM.from_pretrained(model_checkpoint)
+            lora_config = LoraConfig(r = 8,
+                                    lora_alpha = 16,
+                                    lora_dropout = 0.05,
+                                    bias = "none",
+                                    task_type = "CAUSAL_LM")
+            self.model = get_peft_model(self.model, lora_config)
+            self.model.load_state_dict(model_from_checkpoint["model_state_dict"])
+        elif from_checkpoint is False and from_hub is True:
+            config = PeftConfig.from_pretrained(model_from_hub)
+            self.model = AutoModelForCausalLM.from_pretrained(config.base_model_name_or_path)
+            self.model = PeftModel.from_pretrained(self.model, model_from_hub)
+
+    def __call__(self,
+                 instruction: str,
+                 input: str = None,
+                 target: str = None):
+        prompt = self.prompt_process.generate_prompt(instruction = instruction, input = input)
+        inputs = self.tokenizer(prompt, return_tensors = "pt")
+        if inputs["input_ids"][-1] != self.tokenizer.eos_token_id:
+            inputs["input_ids"].append(self.tokenizer.eos_token_id)
+            inputs["attention_mask"].append(1)
+        inputs = {k:v.to(self.device) for k, v in inputs.items()}
+        outputs = self.model.generate(**inputs,
+                                      max_new_tokens = 512,
+                                      no_repeat_ngram_size = 3,
+                                      num_beams = 3,
+                                      top_k = 40,
+                                      top_p = 128,
+                                      bos_token_id = self.tokenizer.bos_token_id,
+                                      eos_token_id = self.tokenizer.eos_token_id,
+                                      pad_token_id = self.tokenizer.pad_token_id,
+                                      early_stopping = True)
+        text = self.tokenizer.batch_decode(outputs, skip_special_tokens = True)[0]
+        response = self.prompt_process.get_response(text)
+        if target is not None:
+            return {"label": target,
+                  "response": response}
+        else:
+            return {"response": response}
+        
