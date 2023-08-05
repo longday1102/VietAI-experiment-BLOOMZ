f8c4c5a9da7ff458ad63f93029a0dfd29470e0ea
Hoàng Long <longhoangai1102@gmail.com>
2023-08-05 17:41:34 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import transformers
 from transformers import AutoModelForCausalLM, AutoTokenizer
-from peft import LoraConfig, PeftConfig, PeftModel, get_peft_model
+from peft import LoraConfig, AutoPeftModelForCausalLM, get_peft_model
 import torch
 
 class Config:
@@ -22,8 +22,6 @@ class Config:
         lora_model = get_peft_model(model, lora_config)
         return lora_model
 
-    def reload_pretrained_model(self, model_weight_path, device_map):
-        config = PeftConfig.from_pretrained(model_weight_path)
-        model = AutoModelForCausalLM.from_pretrained(config.base_model_name_or_path, device_map = device_map, torch_dtype = torch.float16)
-        lora_model = PeftModel.from_pretrained(model, model_weight_path)
+    def reload_pretrained_model(self, model_weight_path, device_map = None):
+        lora_model = AutoPeftModelForCausalLM.from_pretrained(model_weight_path, device_map = device_map)
         return lora_model
