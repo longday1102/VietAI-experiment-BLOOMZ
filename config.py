8da8a412794391fe5b10b0ea0e3fa5c42dea09f2
Hoàng Long <longhoangai1102@gmail.com>
2023-08-05 16:06:50 +0700

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ class Config:
         lora_model = get_peft_model(model, lora_config)
         return lora_model
 
-    def reload_pretrained_model(self, model_weight_path, device_map = None):
+    def reload_pretrained_model(self, model_weight_path, device_map):
         config = PeftConfig.from_pretrained(model_weight_path)
         model = AutoModelForCausalLM.from_pretrained(config.base_model_name_or_path, device_map = device_map, torch_dtype = torch.float16)
         lora_model = PeftModel.from_pretrained(model, model_weight_path)
